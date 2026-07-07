62268fa86963bd862b1dc8fcfa829097d188caa4
Aaron Shenny <aaronshenny102.go@gmail.com>
2026-07-08 00:03:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/picker/",
+  base: process.env.NODE_ENV === "production"
+  ? "/picker/"
+  : "/",
 
   plugins: [react()],
 
